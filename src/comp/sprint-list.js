5d647a8bd498abb999e5a8f4a1d57386c1ffca08
Luis Arce <luis.arce22@gmail.com>
2022-04-22 19:46:24 -0400

    use objects on categories and sprints

--- src/comp/sprint-list.js
+++ src/comp/sprint-list.js
@@ -2,7 +2,7 @@ export default function ({ sprints, total }) {
     return (
         <div style={{ marginBottom: '1rem', width: '20rem' }}>
             {sprints.map(s => (<div style={{ 
-                borderBottom: '1px solid black', backgroundColor: "#F7DFDA", marginBottom: '1px', height: '1.4rem', padding: '10px' }}>{s}</div>))}
+                borderBottom: '1px solid black', backgroundColor: "#F7DFDA", marginBottom: '1px', height: '1.4rem', padding: '10px' }}>{s.qty} {s.cat}</div>))}
             <div style={{ marginTop: '1rem', padding: '10px', backgroundColor: '#FAC0B3' }}>{total}</div>
         </div>
     )
--- src/containers/main-container.js
+++ src/containers/main-container.js
@@ -3,14 +3,21 @@ import CategoryList from "../comp/category-list";
 import SprintInput from "../comp/sprint-input";
 import SprintList from "../comp/sprint-list";
 
+const PUSH_UPS = "PUSH UPS"
+const ABS = "ABS"
+
+const cats_cals = { PUSH_UPS: 10, "ABS": "5" }
+
 export default function () {
     const [inValue, setInValue] = useState('30');
-    const [categories, setCategories] = useState(['PUSH UPS', 'ABS'])
-    const [selectedCategory, setSelectedCategory] = useState("PUSH UPS")
-    const [sprints, setSprints] = useState(["+ 20 PUSH UPS", "+ 50 ABS", "+ 30 PUSH UPS"])
-    const [total, setTotal] = useState("50 PUSH UPS and 50 ABS, 500 Calories")
+    const [categories, setCategories] = useState([{ name: PUSH_UPS }, { name: ABS }])
+    const [selectedCategory, setSelectedCategory] = useState(PUSH_UPS)
+    const [sprints, setSprints] = useState([{ qty: 20, cat: PUSH_UPS }, { qty: 50, cat: ABS }])
+    const [total, setTotal] = useState({ PUSH_UPS: { qty: 20 }, })
+
     const addValue = (e) => {
-        setSprints(sps => [...sps, `+ ${inValue} ${selectedCategory}`])
+        console.log(selectedCategory)
+        setSprints(sps => [...sps, { qty: inValue, cat: selectedCategory }])
         setInValue('')
     }
 
@@ -19,7 +26,7 @@ export default function () {
             <h1>WORKOUT PLUS</h1>
             <SprintInput addValue={addValue} value={inValue} setValue={setInValue} />
             <CategoryList categories={categories} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}></CategoryList>
-            <SprintList sprints={sprints} total={total} />
+            <SprintList sprints={sprints} total={Object.keys(total).reduce((acc, curr) => acc + cats_cals[curr] * total[curr].qty, 0)} />
         </div>
     )
 }
