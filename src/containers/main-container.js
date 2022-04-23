c451eea891d76d97a207bb70cb6ff44581fc223d
Luis Arce <luis.arce22@gmail.com>
2022-04-22 20:03:24 -0400

    More logic to handle the inputs

--- src/containers/main-container.js
+++ src/containers/main-container.js
@@ -6,19 +6,30 @@ import SprintList from "../comp/sprint-list";
 const PUSH_UPS = "PUSH UPS"
 const ABS = "ABS"
 
-const cats_cals = { PUSH_UPS: 10, "ABS": "5" }
+const cats_cals = { 'PUSH UPS': 10, "ABS": 5 }
 
 export default function () {
-    const [inValue, setInValue] = useState('30');
-    const [categories, setCategories] = useState([{ name: PUSH_UPS }, { name: ABS }])
+    const [inValue, setInValue] = useState(30);
+    const [categories] = useState([{ name: PUSH_UPS }, { name: ABS }])
     const [selectedCategory, setSelectedCategory] = useState(PUSH_UPS)
     const [sprints, setSprints] = useState([{ qty: 20, cat: PUSH_UPS }, { qty: 50, cat: ABS }])
-    const [total, setTotal] = useState({ PUSH_UPS: { qty: 20 }, })
+    const [totalValue, setTotalValue] = useState(0);
 
     const addValue = (e) => {
-        console.log(selectedCategory)
-        setSprints(sps => [...sps, { qty: inValue, cat: selectedCategory }])
+        const sprintsAll = [...sprints, { qty: inValue, cat: selectedCategory }]
+        setSprints(sprintsAll)
         setInValue('')
+        updateTotal(sprintsAll)
+    }
+
+    const updateTotal = (sprintsAll) => {
+        const totalAll = { "PUSH UPS": { qty: 0 }, ABS: { qty: 0} }
+        sprintsAll.forEach(s => {
+            totalAll[s.cat].qty += Number(s.qty)
+        })
+
+        const ss = Object.keys(totalAll).reduce((acc, curr) => acc + cats_cals[curr] * totalAll[curr].qty, 0)
+        setTotalValue(ss)
     }
 
     return (
@@ -26,7 +37,7 @@ export default function () {
             <h1>WORKOUT PLUS</h1>
             <SprintInput addValue={addValue} value={inValue} setValue={setInValue} />
             <CategoryList categories={categories} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}></CategoryList>
-            <SprintList sprints={sprints} total={Object.keys(total).reduce((acc, curr) => acc + cats_cals[curr] * total[curr].qty, 0)} />
+            <SprintList sprints={sprints} total={totalValue} />
         </div>
     )
 }
